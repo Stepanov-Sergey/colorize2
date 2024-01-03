1f7e562e40e761ca2e38b69e2f2829a35fbd43e3
Sergey Stepanov <sergey.stepanov@lenta.ru>
2024-01-03 22:59:08 +0300

    Update colorize.js

--- colorize.js
+++ colorize.js
@@ -1,13 +1,24 @@
 class Colorize {
   static fg = {
+    black: (text) => `\x1b[30m${text}\x1b[0m`,
     red: (text) => `\x1b[31m${text}\x1b[0m`,
     green: (text) => `\x1b[32m${text}\x1b[0m`,
+    yellow: (text) => `\x1b[33m${text}\x1b[0m`,
     blue: (text) => `\x1b[34m${text}\x1b[0m`,
+    magenta: (text) => `\x1b[35m${text}\x1b[0m`,
+    cyan: (text) => `\x1b[36m${text}\x1b[0m`,
+    white: (text) => `\x1b[37m${text}\x1b[0m`,
     // Add more colors here as needed
   };
   static bg = {
+    black: (text) => `\x1b[40m${text}\x1b[0m`,
+    red: (text) => `\x1b[41m${text}\x1b[0m`,
     green: (text) => `\x1b[42m${text}\x1b[0m`,
     yellow: (text) => `\x1b[43m${text}\x1b[0m`,
+    blue: (text) => `\x1b[44m${text}\x1b[0m`,
+    magenta: (text) => `\x1b[45m${text}\x1b[0m`,
+    cyan: (text) => `\x1b[46m${text}\x1b[0m`,
+    white: (text) => `\x1b[47m${text}\x1b[0m`,
     // Add more background colors here as needed
   };
 
