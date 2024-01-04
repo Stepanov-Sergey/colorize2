e442ffaf4357ce87ceb63be3d0e8e57fe2116f03
Sergey Stepanov <sergey.stepanov@lenta.ru>
2024-01-04 11:18:43 +0300

    Update colorize.js

--- colorize.js
+++ colorize.js
@@ -3,19 +3,14 @@ static fgcolor = {
   black: 30,
   red: 31,
   green: 32,
+  grass:64,
   yellow: 33,
   blue: 34,
   magenta: 35,
   cyan: 36,
   white: 37,
   gray: 90,
-  bright_red: 91,
-  bright_green: 92,
-  bright_yellow: 93,
-  bright_blue: 94,
-  bright_magenta: 95,
-  bright_cyan: 96,
-  bright_white: 97
+  orange:202,
   // Добавьте больше цветов здесь при необходимости
 };
 
@@ -29,13 +24,7 @@ static bgcolor = {
   cyan: 46,
   white: 47,
   gray: 100,
-  pink: 101,
-  bright_green: 102,
-  bright_yellow: 103,
-  bright_blue: 104,
-  bright_magenta: 105,
-  bright_cyan: 106,
-  bright_white: 107
+  pink: 101
   // Добавьте больше цветов здесь при необходимости
 };
 
